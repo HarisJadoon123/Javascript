8d001e608013bca663b6932badcd88ee55f66555
Haris Jadoon <harisjadoo5@gmail.com>
2026-02-05 17:39:56 +0500

    Two Task Completed

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("HARIS JADOON");
+document.getElementById("demo").innerHTML = "My First JavaScript";
+
+
+function myFunction1(e) {
+    e.preventDefault();
+    let ageInput = document.getElementById("ageInput").value;
+    if(!/^\d+$/.test(ageInput))
+        alert('Invalid Input');
+    else
+    {
+        let ageOutput = ageInput * 365;
+        document.getElementById("ageOutput").value = ageOutput + " Days";
+    }
+}
+function myFunction2(e) {
+    e.preventDefault();
+    let hoursInput = document.getElementById("hoursInput").value;
+    let hoursOutput = hoursInput * 3600;
+    document.getElementById("hoursOutput").value = hoursOutput + " Seconds";
+}
