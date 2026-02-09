f396e50f49d074424b1a43fadd4e03c501598e9c
Haris Jadoon <harisjadoo5@gmail.com>
2026-02-09 17:55:18 +0500

    $ tasks Completed

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-console.log("HARIS JADOON");
-document.getElementById("demo").innerHTML = "My First JavaScript";
+
 
 
 function myFunction1(e) {
@@ -22,4 +21,39 @@ function myFunction2(e) {
     {
     let hoursOutput = hoursInput * 3600;
     document.getElementById("hoursOutput").value = hoursOutput + " Seconds";
+
 }}
+
+
+function myFunction3(e) {
+    e.preventDefault();
+    let numbers = document.getElementById("num").value;             // 1. Numbers input lo
+
+    let findNum = Number(document.getElementById("num2").value);        // 2. Search number lo
+
+    let arr = numbers.split(",").map(Number);                           // 3. Array banao (map use)
+
+    let index = arr.indexOf(findNum);                                       // 4. Index dhoondo
+
+    if (index >= 0 && index < arr.length - 1)                                 // 5. Result dikhao
+        document.getElementById("output").value = arr[index + 1];
+     else 
+        document.getElementById("output").value = findNum + 1;
+}
+
+function myFunction4(e) {
+    e.preventDefault(); 
+    let name = document.getElementById("nameInput").value;
+    if (name.trim() === "") {
+        document.getElementById("nameOutput").value = "Please enter a name";
+        return;
+    }
+    let formattedName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    document.getElementById("nameOutput").value = formattedName;
+}
+
+function myFunction5{
+    e.preventDefault();
+    
+}
+
